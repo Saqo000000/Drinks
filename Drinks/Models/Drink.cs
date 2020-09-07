09df1b66caba085970df0651d0960a855cec5bff
saqo <sevad999@mail.ru>
2020-09-07 21:23:56 +0400

    Adding Some Modeles in folder Models for  muving to dll file

---
+++ Drinks/Models/Drink.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Drinks.Models
+{
+    public class Drink
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public int CategoryID { get; set; }
+        public DrinkCategory Category { get; set; }
+
+        public List<DrinkMenu> DrinkMenus { get; set; }
+    }
+}
---
+++ Drinks/Models/Menu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Drinks.Models
+{
+    public class Menu
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<DrinkMenu> DrinkMenus { get; set; }    //= new List<DrinkMenu>();
+    }
+}
